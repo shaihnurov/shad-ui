1150c94a79766c63ebcced8da7d25b513cb6f45a
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-12 15:41:43 +0800

    docs(rounded-image): add xaml docs

--- src/ShadUI/Controls/RoundedImage.cs
+++ src/ShadUI/Controls/RoundedImage.cs
@@ -4,19 +4,40 @@ using Avalonia.Media;
 
 namespace ShadUI.Controls;
 
+/// <summary>
+/// Custom Image control with rounded corners.
+/// </summary>
 public class RoundedImage : Image
 {
+    /// <summary>
+    /// Attached property for setting the corner radius of the image.
+    /// </summary>
     public static readonly AttachedProperty<double> CornerRadiusProperty =
         AvaloniaProperty.RegisterAttached<RoundedImage, double>(
             "CornerRadius", typeof(RoundedImage), 5);
 
+    /// <summary>
+    /// Sets the corner radius of the image.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <param name="parameter"></param>
     public static void SetCornerRadius(AvaloniaObject element, double parameter)
     {
         element.SetValue(CornerRadiusProperty, parameter);
     }
 
+    /// <summary>
+    /// Gets the corner radius of the image.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
     public static double GetCornerRadius(AvaloniaObject element) => element.GetValue(CornerRadiusProperty);
 
+    /// <summary>
+    /// Measures the control.
+    /// </summary>
+    /// <param name="availableSize">The available size.</param>
+    /// <returns>The desired size of the control.</returns>
     protected override Size MeasureOverride(Size availableSize)
     {
         var source = Source;
