8993927ad2e6e02da659eaa13673e41612b0ea6f
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-12 22:38:08 +0800

    fix(demo): improve code block

---
+++ src/ShadUI.Demo/Controls/ControlBlock.axaml.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace ShadUI.Demo.Controls;
+
+public class ControlBlock : ContentControl
+{
+    public static readonly StyledProperty<string?> CodeProperty =
+        AvaloniaProperty.Register<CodeTextBlock, string?>(nameof(Code));
+
+    public string? Code
+    {
+        get => GetValue(CodeProperty);
+        set => SetValue(CodeProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> LanguageProperty =
+        AvaloniaProperty.Register<CodeTextBlock, string?>(nameof(Language), defaultValue: "xaml");
+
+    public string? Language
+    {
+        get => GetValue(LanguageProperty);
+        set => SetValue(LanguageProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> TitleProperty =
+        AvaloniaProperty.Register<CodeTextBlock, string?>(nameof(Title));
+
+    public string? Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    
+    public static readonly StyledProperty<double?> CodeBlockHeightProperty =
+        AvaloniaProperty.Register<CodeTextBlock, double?>(nameof(CodeBlockHeight));
+    
+    public double? CodeBlockHeight
+    {
+        get => GetValue(CodeBlockHeightProperty);
+        set => SetValue(CodeBlockHeightProperty, value);
+    }
+}
