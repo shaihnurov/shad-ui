9e92e0dfc1b63cc8e3ad5874043eb92c261d2259
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-13 12:15:40 +0800

    fix: code view has no code highlights

--- src/ShadUI.Demo/ViewModels/TypographyViewModel.cs
+++ src/ShadUI.Demo/ViewModels/TypographyViewModel.cs
@@ -6,22 +6,26 @@ public sealed partial class TypographyViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _h1Code = """
-                             <TextBlock TextWrapping="Wrap" Classes="h1" Text="Taxing Laughter: The Joke Tax Chronicles" />
+                             <TextBlock TextWrapping="Wrap" Classes="h1" 
+                                        Text="Taxing Laughter: The Joke Tax Chronicles" />
                              """;
     
     [ObservableProperty]
     private string _h2Code = """
-                             <TextBlock TextWrapping="Wrap" Classes="h2" Text="The People of the Kingdom" />
+                             <TextBlock TextWrapping="Wrap" Classes="h2" 
+                                        Text="The People of the Kingdom" />
                              """;
     
     [ObservableProperty]
     private string _h3Code = """
-                             <TextBlock TextWrapping="Wrap" Classes="h3" Text="The Joke Tax" />
+                             <TextBlock TextWrapping="Wrap" Classes="h3" 
+                                        Text="The Joke Tax" />
                              """;
     
     [ObservableProperty]
     private string _h4Code = """
-                             <TextBlock TextWrapping="Wrap" Classes="h4" Text="People stopped telling jokes" />
+                             <TextBlock TextWrapping="Wrap" Classes="h4" 
+                                        Text="People stopped telling jokes" />
                              """;
     
     [ObservableProperty]
@@ -44,21 +48,25 @@ public sealed partial class TypographyViewModel : ViewModelBase
     
     [ObservableProperty]
     private string _smallCode = """
-                             <TextBlock TextWrapping="Wrap" Classes="Small" Text="Email address" />
+                             <TextBlock TextWrapping="Wrap" Classes="Small" 
+                                        Text="Email address" />
                              """;
     
     [ObservableProperty]
     private string _captionCode = """
-                             <TextBlock TextWrapping="Wrap" TextAlignment="Center" Classes="Caption" Text="Username" />
+                             <TextBlock TextWrapping="Wrap" TextAlignment="Center" Classes="Caption" 
+                                        Text="Username" />
                              """;
     
     [ObservableProperty]
     private string _mutedCode = """
-                             <TextBlock TextWrapping="Wrap" Classes="Muted" Text="Enter your email address" />
+                             <TextBlock TextWrapping="Wrap" Classes="Muted" 
+                                        Text="Enter your email address" />
                              """;
     
     [ObservableProperty]
     private string _errorCode = """
-                             <TextBlock TextWrapping="Wrap" Classes="Caption Error" Text="Invalid email address" />
+                             <TextBlock TextWrapping="Wrap" Classes="Caption Error" 
+                                        Text="Invalid email address" />
                              """;
 }
