edd3acdb271a8e68875857de0927471b98733433
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-23 17:42:01 +0800

    feat: implement breakpoint component

---
+++ src/ShadUI/Breakpoints/View.axaml.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace ShadUI.Breakpoints;
+
+/// <summary>
+/// Represents a view that can change its layout based on breakpoints.
+/// </summary>
+public class View : Grid
+{
+    /// <summary>
+    /// Defines the <see cref="Breakpoint"/> property.
+    /// </summary>
+    public static readonly StyledProperty<Breakpoint> BreakpointProperty =
+        AvaloniaProperty.Register<ViewPort, Breakpoint>(nameof(Breakpoint));
+
+    /// <summary>
+    /// Gets or sets the breakpoint for the view.
+    /// </summary>
+    public Breakpoint Breakpoint
+    {
+        get => GetValue(BreakpointProperty);
+        set => SetValue(BreakpointProperty, value);
+    }
+
+    /// <summary>
+    /// Defines the <see cref="Invert"/> property.
+    /// </summary>
+    public static readonly StyledProperty<bool> InvertProperty = AvaloniaProperty.Register<ViewPort, bool>(
+        nameof(Invert));
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to invert the breakpoint logic.
+    /// </summary>
+    public bool Invert
+    {
+        get => GetValue(InvertProperty);
+        set => SetValue(InvertProperty, value);
+    }
+}
---
+++ src/ShadUI/Breakpoints/ViewPort.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls;
+
+namespace ShadUI.Breakpoints;
+
+/// <summary>
+///     Container for bound content that will only be visible when the specified breakpoint is active.
+/// </summary>
+public class ViewPort : Panel
+{
+}
