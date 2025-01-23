d65121775d1d912ca468737c55c3fb1eb19cf95f
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-23 23:59:54 +0800

    feat(demo): implement dashboard

---
+++ src/ShadUI.Demo/Controls/Dashboard/OverviewCard.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace ShadUI.Demo.Controls.Dashboard;
+
+public class OverviewCard : TemplatedControl
+{
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<OverviewCard, string>(nameof(Title));
+
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly StyledProperty<string> ValueProperty =
+        AvaloniaProperty.Register<OverviewCard, string>(nameof(Value));
+
+    public string Value
+    {
+        get => GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    public static readonly StyledProperty<string> HintProperty =
+        AvaloniaProperty.Register<OverviewCard, string>(nameof(Hint));
+
+    public string Hint
+    {
+        get => GetValue(HintProperty);
+        set => SetValue(HintProperty, value);
+    }
+
+    public static readonly StyledProperty<object> IconProperty = 
+        AvaloniaProperty.Register<OverviewCard, object>(nameof(Icon));
+
+    public object Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
--- src/ShadUI.Demo/ViewModels/DashboardViewModel.cs
+++ src/ShadUI.Demo/ViewModels/DashboardViewModel.cs
@@ -1,11 +1,101 @@
-﻿using System;
+﻿using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using ShadUI.Themes;
+using SkiaSharp;
 
 namespace ShadUI.Demo.ViewModels;
 
-public sealed class DashboardViewModel : ViewModelBase
+public sealed partial class DashboardViewModel : ViewModelBase
 {
+    private readonly ThemeWatcher _themeWatcher;
+
+    [ObservableProperty]
+    private static SolidColorPaint _tooltipTextPaint = new()
+    {
+        Color = SKColors.Black,
+        SKTypeface = SKTypeface.FromFamilyName("Inter")
+    };
+
+    public DashboardViewModel(ThemeWatcher themeWatcher)
+    {
+        _themeWatcher = themeWatcher;
+        _themeWatcher.ThemeChanged += (_, colors) => UpdateThemeColors(colors);
+    }
+
     public void Initialize()
     {
-        Console.WriteLine("DashboardViewModel initialized.");
+        UpdateThemeColors(_themeWatcher.ThemeColors);
     }
+
+    private void UpdateThemeColors(ThemeColors colors)
+    {
+        UpdateAxesLabelPaints(colors);
+        UpdateSeriesFill(colors.PrimaryColor);
+    }
+
+    private void UpdateAxesLabelPaints(ThemeColors colors)
+    {
+        var foreground = new SKColor(
+            colors.ForegroundColor.R,
+            colors.ForegroundColor.G,
+            colors.ForegroundColor.B,
+            colors.ForegroundColor.A);
+        var foregroundPaint = new SolidColorPaint
+        {
+            Color = foreground,
+            SKTypeface = SKTypeface.FromFamilyName("Inter")
+        };
+
+        XAxes[0].LabelsPaint = foregroundPaint;
+        YAxes[0].LabelsPaint = foregroundPaint;
+    }
+    
+    private void UpdateSeriesFill(Color color)
+    {
+        var skColor = new SKColor(color.R, color.G, color.B, color.A);
+        ((ColumnSeries<double>) Series[0]).Fill = new SolidColorPaint(skColor);
+    }
+
+    public ISeries[] Series { get; set; } =
+    [
+        new ColumnSeries<double>
+        {
+            Values = [1300, 2700, 2950, 1750, 1259, 4490, 5500, 1550, 1540, 4450, 3000, 3200],
+            Fill = new SolidColorPaint(SKColors.Transparent)
+        }
+    ];
+
+    public Axis[] XAxes { get; set; } =
+    [
+        new()
+        {
+            Labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            LabelsPaint = new SolidColorPaint
+            {
+                Color = SKColors.Gray,
+                SKTypeface = SKTypeface.FromFamilyName("Inter")
+            },
+            TextSize = 12,
+            MinStep = 1
+        }
+    ];
+
+    public Axis[] YAxes { get; set; } =
+    [
+        new()
+        {
+            Labeler = Labelers.Currency,
+            LabelsPaint = new SolidColorPaint
+            {
+                Color = SKColors.Gray,
+                SKTypeface = SKTypeface.FromFamilyName("Inter")
+            },
+            TextSize = 12,
+            MinStep = 1500,
+            ShowSeparatorLines = false
+        }
+    ];
 }
