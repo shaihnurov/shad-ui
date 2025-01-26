b57f662c92adb68746b6f0f252621bba1fcaee8c
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-01-26 13:52:49 +0800

    fix(demo): show tooltip when sidebar is not expanded

---
+++ src/ShadUI.Demo/Converters/BooleanToNullOrStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ShadUI.Demo.Converters;
+
+public class BooleanToNullOrStringConverter : IValueConverter
+{
+    public static BooleanToNullOrStringConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var stringValue = parameter?.ToString();
+
+        if (value is bool booleanValue) return booleanValue ? null : stringValue;
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
