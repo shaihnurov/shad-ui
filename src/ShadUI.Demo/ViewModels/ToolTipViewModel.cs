abc5a57567b9295ca962c9082fa1c799eaf7836f
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-02-02 21:39:39 +0800

    fix(demo): add tooltip code sample

--- src/ShadUI.Demo/ViewModels/ToolTipViewModel.cs
+++ src/ShadUI.Demo/ViewModels/ToolTipViewModel.cs
@@ -6,6 +6,13 @@ public sealed partial class ToolTipViewModel: ViewModelBase
 {
     [ObservableProperty]
     private string _usageCode ="""
-                               
+                               <StackPanel VerticalAlignment="Center" HorizontalAlignment="Center">
+                                   <Button ToolTip.Tip="Add to library"
+                                           ToolTip.Placement="Top"
+                                           ToolTip.VerticalOffset="-5"
+                                           Classes="Outline">
+                                       Hover
+                                   </Button>
+                               </StackPanel>
                                """;
 }
