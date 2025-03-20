4bdf356136d6b22b6947b080627bf6a2fd47b457
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-03-20 10:50:47 +0800

    fix(demo): improve button page

--- src/ShadUI.Demo/ViewModels/ButtonsViewModel.cs
+++ src/ShadUI.Demo/ViewModels/ButtonsViewModel.cs
@@ -1,23 +1,32 @@
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ShadUI.Demo.ViewModels;
 
 public sealed partial class ButtonsViewModel : ViewModelBase
 {
     [ObservableProperty]
+    private bool _isBusy;
+
+    [RelayCommand]
+    private async Task SubmitAsync()
+    {
+        IsBusy = true;
+        await Task.Delay(5000);
+        IsBusy = false;
+    }
+
+    [ObservableProperty]
     private string _primaryCode = """
                                   <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                      <Button Classes="Primary" Content="Primary" />
-                                      <Button Classes="Primary" Content="Edit">
+                                      <Button Classes="Primary" Content="Primary"
+                                              extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}" />
+                                      <Button Classes="Primary" Content="Edit" extensions:Button.ShowProgress="{Binding IsBusy}"
+                                              Command="{Binding SubmitCommand}">
                                           <extensions:Button.Icon>
                                               <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                            StrokeBrush="{DynamicResource ForegroundColor}" />
-                                          </extensions:Button.Icon>
-                                      </Button>
-                                      <Button Classes="Primary" extensions:Button.ShowProgress="True" Content="Saving...">
-                                          <extensions:Button.Icon>
-                                              <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                            StrokeBrush="{DynamicResource ForegroundColor}" />
+                                                            StrokeBrush="{DynamicResource PrimaryForegroundColor}" />
                                           </extensions:Button.Icon>
                                       </Button>
                                       <Button Classes="Primary" IsEnabled="False" Content="Disable" />
@@ -27,17 +36,13 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _secondaryCode = """
                                     <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                        <Button Classes="Secondary" Content="Secondary" />
-                                        <Button Classes="Secondary" Content="Edit">
+                                        <Button Classes="Secondary" Content="Secondary"
+                                                extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}" />
+                                        <Button Classes="Secondary" Content="Edit"
+                                                extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}">
                                             <extensions:Button.Icon>
                                                 <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                              StrokeBrush="{DynamicResource ForegroundColor}" />
-                                            </extensions:Button.Icon>
-                                        </Button>
-                                        <Button Classes="Secondary" extensions:Button.ShowProgress="True" Content="Saving...">
-                                            <extensions:Button.Icon>
-                                                <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                              StrokeBrush="{DynamicResource ForegroundColor}" />
+                                                              StrokeBrush="{DynamicResource SecondaryForegroundColor}" />
                                             </extensions:Button.Icon>
                                         </Button>
                                         <Button Classes="Secondary" IsEnabled="False" Content="Disable" />
@@ -47,14 +52,10 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _destructiveCode = """
                                       <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                          <Button Classes="Destructive" Content="Destructive" />
-                                          <Button Classes="Destructive" Content="Delete">
-                                              <extensions:Button.Icon>
-                                                  <icons:Lucide Icon="Trash" StrokeThickness="1.5" Width="10" Height="16"
-                                                                StrokeBrush="{DynamicResource DestructiveForegroundColor}" />
-                                              </extensions:Button.Icon>
-                                          </Button>
-                                          <Button Classes="Destructive" extensions:Button.ShowProgress="True" Content="Saving...">
+                                          <Button Classes="Destructive" Content="Destructive"
+                                                  extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}" />
+                                          <Button Classes="Destructive" Content="Delete"
+                                                  extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}">
                                               <extensions:Button.Icon>
                                                   <icons:Lucide Icon="Trash" StrokeThickness="1.5" Width="10" Height="16"
                                                                 StrokeBrush="{DynamicResource DestructiveForegroundColor}" />
@@ -67,14 +68,10 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _outlineCode = """
                                   <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                      <Button Classes="Outline" Content="Outline" />
-                                      <Button Classes="Outline" Content="Edit">
-                                          <extensions:Button.Icon>
-                                              <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                            StrokeBrush="{DynamicResource ForegroundColor}" />
-                                          </extensions:Button.Icon>
-                                      </Button>
-                                      <Button Classes="Outline" extensions:Button.ShowProgress="True" Content="Saving...">
+                                      <Button Classes="Outline" Content="Outline"
+                                              extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}" />
+                                      <Button Classes="Outline" Content="Edit" extensions:Button.ShowProgress="{Binding IsBusy}"
+                                              Command="{Binding SubmitCommand}">
                                           <extensions:Button.Icon>
                                               <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
                                                             StrokeBrush="{DynamicResource ForegroundColor}" />
@@ -87,14 +84,10 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _ghostCode = """
                                 <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                    <Button Classes="Ghost" Content="Ghost" />
-                                    <Button Classes="Ghost" Content="Edit">
-                                        <extensions:Button.Icon>
-                                            <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
-                                                          StrokeBrush="{DynamicResource ForegroundColor}" />
-                                        </extensions:Button.Icon>
-                                    </Button>
-                                    <Button Classes="Ghost" extensions:Button.ShowProgress="True" Content="Saving...">
+                                    <Button Classes="Ghost" Content="Ghost" extensions:Button.ShowProgress="{Binding IsBusy}"
+                                            Command="{Binding SubmitCommand}" />
+                                    <Button Classes="Ghost" Content="Edit" extensions:Button.ShowProgress="{Binding IsBusy}"
+                                            Command="{Binding SubmitCommand}">
                                         <extensions:Button.Icon>
                                             <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="10" Height="16"
                                                           StrokeBrush="{DynamicResource ForegroundColor}" />
@@ -107,14 +100,8 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _iconCode = """
                                <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                   <Button Classes="Icon" BorderBrush="{DynamicResource BorderColor}">
-                                       <extensions:Button.Icon>
-                                           <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="16"
-                                                         StrokeBrush="{DynamicResource ForegroundColor}" />
-                                       </extensions:Button.Icon>
-                                   </Button>
-                                   <Button Classes="Icon" extensions:Button.ShowProgress="True"
-                                           BorderBrush="{DynamicResource BorderColor}">
+                                   <Button Classes="Icon" BorderBrush="{DynamicResource BorderColor}"
+                                           extensions:Button.ShowProgress="{Binding IsBusy}" Command="{Binding SubmitCommand}">
                                        <extensions:Button.Icon>
                                            <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="16"
                                                          StrokeBrush="{DynamicResource ForegroundColor}" />
@@ -132,21 +119,16 @@ public sealed partial class ButtonsViewModel : ViewModelBase
     [ObservableProperty]
     private string _destructiveIconCode = """
                                           <StackPanel Spacing="8" Orientation="Horizontal" HorizontalAlignment="Center">
-                                              <Button Classes="DestructiveIcon">
+                                              <Button Classes="DestructiveIcon" extensions:Button.ShowProgress="{Binding IsBusy}"
+                                                      Command="{Binding SubmitCommand}">
                                                   <extensions:Button.Icon>
-                                                      <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="16"
-                                                                    StrokeBrush="{DynamicResource DestructiveForegroundColor}" />
-                                                  </extensions:Button.Icon>
-                                              </Button>
-                                              <Button Classes="DestructiveIcon" extensions:Button.ShowProgress="True">
-                                                  <extensions:Button.Icon>
-                                                      <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="16"
+                                                      <icons:Lucide Icon="Trash" StrokeThickness="1.5" Width="16"
                                                                     StrokeBrush="{DynamicResource DestructiveForegroundColor}" />
                                                   </extensions:Button.Icon>
                                               </Button>
                                               <Button Classes="DestructiveIcon" IsEnabled="False">
                                                   <extensions:Button.Icon>
-                                                      <icons:Lucide Icon="Pencil" StrokeThickness="1.5" Width="16"
+                                                      <icons:Lucide Icon="Trash" StrokeThickness="1.5" Width="16"
                                                                     StrokeBrush="{DynamicResource DestructiveForegroundColor}" />
                                                   </extensions:Button.Icon>
                                               </Button>
