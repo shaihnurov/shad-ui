4392ae88aa039a1ba265a11fb7678da13b3f6399
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-03-20 10:49:49 +0800

    fix(button): improve button layout

--- src/ShadUI/Converters/BooleanConverters.cs
+++ src/ShadUI/Converters/BooleanConverters.cs
@@ -6,6 +6,9 @@ namespace ShadUI.Converters;
 
 internal static class BooleanConverters
 {
+    public static readonly IValueConverter ToInverseOpacity =
+        new FuncValueConverter<bool, int>((value) => value ? 0 : 1);
+    
     public static readonly IValueConverter ToLoading =
         new FuncValueConverter<bool, object>((value) => value ? new Loading() : new Panel());
 }
