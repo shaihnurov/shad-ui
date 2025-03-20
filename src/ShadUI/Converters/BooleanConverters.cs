a5df1c6bfe0e6ada02cbe77537edef249d036b1a
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-03-20 10:44:32 +0800

    fix: simplify loading converter instance

---
+++ src/ShadUI/Converters/BooleanConverters.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using ShadUI.Controls;
+
+namespace ShadUI.Converters;
+
+internal static class BooleanConverters
+{
+    public static readonly IValueConverter ToLoading =
+        new FuncValueConverter<bool, object>((value) => value ? new Loading() : new Panel());
+}
