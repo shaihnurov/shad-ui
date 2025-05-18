f1035743f7e2f7b09446faa03e495c7daf03ed1f
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-05-18 19:09:33 +0800

    feat: implement SimpleDropdown component

---
+++ src/ShadUI/Controls/SimpleDropdown.axaml.cs
@@ -0,0 +1,255 @@
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace ShadUI.Controls;
+
+/// <summary>
+///     A simple dropdown control that displays a list of items in a popup when triggered.
+/// </summary>
+[PseudoClasses(":open", ":pressed")]
+public class SimpleDropdown : ItemsControl
+{
+    /// <summary>
+    ///     Defines the <see cref="TriggerContent" /> property.
+    /// </summary>
+    public static readonly StyledProperty<object?> TriggerContentProperty =
+        AvaloniaProperty.Register<SimpleDropdown, object?>(
+            nameof(TriggerContent));
+
+    /// <summary>
+    ///     Gets or sets the content to display in the trigger button.
+    /// </summary>
+    public object? TriggerContent
+    {
+        get => GetValue(TriggerContentProperty);
+        set => SetValue(TriggerContentProperty, value);
+    }
+
+    /// <summary>
+    ///     Defines the <see cref="MenuLabel" /> property.
+    /// </summary>
+    public static readonly StyledProperty<object?> MenuLabelProperty =
+        AvaloniaProperty.Register<SimpleDropdown, object?>(
+            nameof(MenuLabel));
+
+    /// <summary>
+    ///     Gets or sets the label displayed at the top of the dropdown menu.
+    /// </summary>
+    public object? MenuLabel
+    {
+        get => GetValue(MenuLabelProperty);
+        set => SetValue(MenuLabelProperty, value);
+    }
+
+    /// <summary>
+    ///     Defines the <see cref="IsDropDownOpen" /> property.
+    /// </summary>
+    public static readonly StyledProperty<bool> IsDropDownOpenProperty =
+        AvaloniaProperty.Register<SimpleDropdown, bool>(
+            nameof(IsDropDownOpen));
+
+    /// <summary>
+    ///     Gets or sets a value that indicates whether the dropdown is currently open.
+    /// </summary>
+    public bool IsDropDownOpen
+    {
+        get => GetValue(IsDropDownOpenProperty);
+        set => SetValue(IsDropDownOpenProperty, value);
+    }
+
+    /// <summary>
+    ///     Defines the <see cref="PopupPlacement" /> property.
+    /// </summary>
+    public static readonly StyledProperty<PlacementMode> PopupPlacementProperty =
+        AvaloniaProperty.Register<SimpleDropdown, PlacementMode>(
+            nameof(PopupPlacement));
+
+    /// <summary>
+    ///     Gets or sets the placement mode of the popup relative to the trigger.
+    /// </summary>
+    public PlacementMode PopupPlacement
+    {
+        get => GetValue(PopupPlacementProperty);
+        set => SetValue(PopupPlacementProperty, value);
+    }
+
+    /// <summary>
+    ///     Defines the <see cref="PopupVerticalOffset" /> property.
+    /// </summary>
+    public static readonly StyledProperty<double> PopupVerticalOffsetProperty =
+        AvaloniaProperty.Register<SimpleDropdown, double>(
+            nameof(PopupVerticalOffset));
+
+    /// <summary>
+    ///     Gets or sets the vertical offset of the popup relative to its default position.
+    /// </summary>
+    public double PopupVerticalOffset
+    {
+        get => GetValue(PopupVerticalOffsetProperty);
+        set => SetValue(PopupVerticalOffsetProperty, value);
+    }
+
+    /// <summary>
+    ///     Defines the <see cref="PopupWidth" /> property.
+    /// </summary>
+    public static readonly StyledProperty<double> PopupWidthProperty =
+        AvaloniaProperty.Register<SimpleDropdown, double>(
+            nameof(PopupWidth));
+
+    /// <summary>
+    ///     Gets or sets the width of the popup.
+    /// </summary>
+    public double PopupWidth
+    {
+        get => GetValue(PopupWidthProperty);
+        set => SetValue(PopupWidthProperty, value);
+    }
+
+    /// <summary>
+    ///     Called when the template is applied to the control.
+    /// </summary>
+    /// <param name="e">Contains event data.</param>
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        DetachEventHandlers();// to avoid duplicate event handlers
+        AttachEventHandlers(e.NameScope);
+    }
+
+    private void AttachEventHandlers(INameScope nameScope)
+    {
+        if (nameScope.Find<Border>("root") is { } rootBorder)
+        {
+            rootBorder.PointerPressed += OnRootBorderPressed;
+            rootBorder.PointerReleased += OnRootBorderReleased;
+            rootBorder.PointerCaptureLost += OnRootBorderCaptureLost;
+        }
+
+        if (nameScope.Find<ItemsPresenter>("PART_ItemsPresenter") is { } itemsPresenter)
+        {
+            itemsPresenter.AttachedToVisualTree += OnItemsPresenterAttached;
+        }
+    }
+
+    private void DetachEventHandlers()
+    {
+        if (this.GetTemplateChildren().FirstOrDefault(x => x.Name == "root") is Border rootBorder)
+        {
+            rootBorder.PointerPressed -= OnRootBorderPressed;
+            rootBorder.PointerReleased -= OnRootBorderReleased;
+            rootBorder.PointerCaptureLost -= OnRootBorderCaptureLost;
+        }
+
+        if (this.GetTemplateChildren().FirstOrDefault(x => x.Name == "PART_ItemsPresenter") is ItemsPresenter
+            itemsPresenter)
+        {
+            itemsPresenter.AttachedToVisualTree -= OnItemsPresenterAttached;
+        }
+
+        // Remove click handlers from all items
+        foreach (var item in Items)
+        {
+            if (item is Control control)
+            {
+                control.RemoveHandler(Button.ClickEvent, OnMenuItemClicked);
+            }
+        }
+    }
+
+    private void OnItemsPresenterAttached(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        UpdateItemsClickHandler();
+    }
+
+    /// <summary>
+    ///     Called when a property value changes.
+    /// </summary>
+    /// <param name="change">Contains event data.</param>
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == ItemsSourceProperty)
+        {
+            UpdateItemsClickHandler();
+        }
+        else if (change.Property == IsDropDownOpenProperty)
+        {
+            var isOpen = (bool)change.NewValue!;
+            PseudoClasses.Set(":open", isOpen);
+        }
+    }
+
+    private void UpdateItemsClickHandler()
+    {
+        foreach (var item in Items)
+        {
+            if (item is not Control control) continue;
+
+            control.RemoveHandler(Button.ClickEvent, OnMenuItemClicked);
+            control.AddHandler(Button.ClickEvent, OnMenuItemClicked);
+        }
+    }
+
+    private void OnMenuItemClicked(object? sender, RoutedEventArgs e)
+    {
+        IsDropDownOpen = false;
+    }
+
+    private void OnRootBorderPressed(object? sender, PointerPressedEventArgs e)
+    {
+        PseudoClasses.Set(":pressed", true);
+        IsDropDownOpen = !IsDropDownOpen;
+        e.Handled = true;
+    }
+
+    private void OnRootBorderReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        PseudoClasses.Set(":pressed", false);
+        e.Handled = true;
+    }
+
+    private void OnRootBorderCaptureLost(object? sender, PointerCaptureLostEventArgs e)
+    {
+        PseudoClasses.Set(":pressed", false);
+    }
+
+    /// <summary>
+    ///     Called when the control is unloaded from the visual tree.
+    /// </summary>
+    /// <param name="e">Contains event data.</param>
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+        DetachEventHandlers();
+    }
+
+    /// <summary>
+    ///     Called when the control is loaded into the visual tree.
+    /// </summary>
+    /// <param name="e">Contains event data.</param>
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+        // Use GetTemplateChildren for OnLoaded since we don't have access to a proper INameScope
+        if (this.GetTemplateChildren().FirstOrDefault(x => x.Name == "root") is Border rootBorder)
+        {
+            rootBorder.PointerPressed += OnRootBorderPressed;
+            rootBorder.PointerReleased += OnRootBorderReleased;
+            rootBorder.PointerCaptureLost += OnRootBorderCaptureLost;
+        }
+
+        if (this.GetTemplateChildren().FirstOrDefault(x => x.Name == "PART_ItemsPresenter") is ItemsPresenter
+            itemsPresenter)
+        {
+            itemsPresenter.AttachedToVisualTree += OnItemsPresenterAttached;
+        }
+    }
+}
