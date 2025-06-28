ee4d43940277affb6f253cae0029a09a6a7c17ea
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-06-28 11:45:23 +0800

    fix(demo): improve RightWindowTitleBarContent controls and behaviors

--- src/ShadUI.Demo/MainWindow.axaml.cs
+++ src/ShadUI.Demo/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
-using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using HotAvalonia;
 using Window = ShadUI.Controls.Window;
 
 namespace ShadUI.Demo;
@@ -13,17 +13,33 @@ public partial class MainWindow : Window
 
         Closing += OnClosing;
         FullscreenButton.Click += OnFullScreen;
-        ExitFullscreenButton.Click += OnExitFullScreen;
     }
+    
+    [AvaloniaHotReload]
+    // ReSharper disable once UnusedMember.Local
+    // Used by Avalonia Hot Reload. Do not remove.
+    private void Initialize()
+    {
+        FullscreenButton.Click -= OnFullScreen;
+        FullscreenButton.Click += OnFullScreen;
+    }
+
+    private WindowState _cacheWindowState = WindowState.Normal;
 
     private void OnFullScreen(object? sender, RoutedEventArgs e)
     {
-        WindowState = WindowState.FullScreen;
-    }
+        if (WindowState != WindowState.FullScreen) _cacheWindowState = WindowState;
 
-    private void OnExitFullScreen(object? sender, RoutedEventArgs e)
-    {
-        ExitFullScreen();
+        if (WindowState == WindowState.FullScreen)
+        {
+            WindowState = _cacheWindowState;
+            ToolTip.SetTip(FullscreenButton, "Fullscreen");
+        }
+        else
+        {
+            WindowState = WindowState.FullScreen;
+            ToolTip.SetTip(FullscreenButton, "Exit Fullscreen");
+        }
     }
 
     private void OnClosing(object? sender, WindowClosingEventArgs e)
